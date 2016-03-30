15154082be3c0e31eb9c3ba485de6813f7a8b97b
Rowan Wing <wingr@github.com>
2016-03-29 19:40:00 -0600

    Adding setup file and removing .pyc files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pywing',
+      version='0.1',
+      description='Python utility functions',
+      url='http://github.com/wingr/pywing',
+      author='Rowan Wing',
+      author_email='wing.rowan@gmail.com',
+      packages=['pywing'],
+      zip_safe=False)
